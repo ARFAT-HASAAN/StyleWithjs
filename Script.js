7be30019942272721338f65613aa3a5f2925d42b
ARFAT-HASAAN <arfuhtv757@gmail.com>
2022-01-01 12:44:52 +0600

    commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+let myText = document.getElementById("Mass");
+myText.style.fontSize = "3rem";
+myText.style.color = "red";
+myText.style.textAlign = "center";
+myText.style.textTransform = "uppercase";
+myText.style.border = "1px solid black";
+myText.style.width = "400px";
+myText.style.margin = "auto";
